5932d7f74d31939cc1388651029911f268e396a9
mrawshan <54669576+mrawshan@users.noreply.github.com>
2021-04-26 16:02:50 +0300

    Adding the changessb

--- assert/script.js
+++ assert/script.js
@@ -10,3 +10,42 @@ hamburgerBtn.addEventListener('click', () => {
 hamburgerBtnExit.addEventListener('click', () => {
 	nav.classList.remove('show-manu');
 });
+
+// For clients slider
+$('.clients-slider').slick({
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	dots: false,
+	arrow: true,
+	autoplay: true,
+	autoplaSpeed: 2000,
+	infinite: true,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false,
+			},
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+			},
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			},
+		},
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	],
+});
