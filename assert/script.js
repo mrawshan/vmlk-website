a871fa2918a46cf6475a53f58242749462da2f6d
mrawshan <54669576+mrawshan@users.noreply.github.com>
2021-05-05 14:07:04 +0300

    Adding the changes

--- assert/script.js
+++ assert/script.js
@@ -1,16 +1,3 @@
-// For hamburger-menu and hamburger-menu-exit
-const hamburgerBtn = document.getElementById('hamburger-cta');
-const nav = document.querySelector('nav');
-const hamburgerBtnExit = document.getElementById('hamburger-exit-cta');
-
-hamburgerBtn.addEventListener('click', () => {
-	nav.classList.add('show-manu');
-});
-
-hamburgerBtnExit.addEventListener('click', () => {
-	nav.classList.remove('show-manu');
-});
-
 // For clients slider
 $('.clients-slider').slick({
 	slidesToShow: 5,
@@ -48,4 +35,50 @@ $('.clients-slider').slick({
 		// settings: "unslick"
 		// instead of a settings object
 	],
+});
+
+// For hamburger-menu and hamburger-menu-exit
+const hamburgerBtn = document.getElementById('hamburger-cta');
+const nav = document.querySelector('nav');
+const hamburgerBtnExit = document.getElementById('hamburger-exit-cta');
+
+hamburgerBtn.addEventListener('click', () => {
+	nav.classList.add('show-manu');
+});
+
+hamburgerBtnExit.addEventListener('click', () => {
+	nav.classList.remove('show-manu');
+});
+
+// Jquery for active headings
+$(document).ready(function () {
+	// First active heading
+	$('.heading-one').click(function () {
+		$('.bottom-line').addClass('bottom-active');
+		$('.bottom-line').removeClass('bottom-active1 bottom-active2 bottom-active3 bottom-active4');
+	});
+
+	// Second active heading
+	$('.heading-two').click(function () {
+		$('.bottom-line').addClass('bottom-active1');
+		$('.bottom-line').removeClass('bottom-active bottom-active2 bottom-active3 bottom-active4');
+	});
+
+	// Third active heading
+	$('.heading-three').click(function () {
+		$('.bottom-line').addClass('bottom-active2');
+		$('.bottom-line').removeClass('bottom-active bottom-active1 bottom-active3 bottom-active4');
+	});
+
+	// Fourth active heading
+	$('.heading-four').click(function () {
+		$('.bottom-line').addClass('bottom-active3');
+		$('.bottom-line').removeClass('bottom-active bottom-active1 bottom-active2 bottom-active4');
+	});
+
+	// Fifth active heading
+	$('.heading-five').click(function () {
+		$('.bottom-line').addClass('bottom-active4');
+		$('.bottom-line').removeClass('bottom-active bottom-active1 bottom-active2 bottom-active3');
+	});
 });
