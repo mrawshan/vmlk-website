6dc8c66699ba39fa8ee6f3d5d9781cdaa0dbfd87
mrawshan <54669576+mrawshan@users.noreply.github.com>
2022-02-25 06:32:51 +0300

    adding the changes

--- assert/script.js
+++ assert/script.js
@@ -1,17 +1,35 @@
-// For hamburger-menu and hamburger-menu-exit
-const hamburgerBtn = document.getElementById('hamburger-cta');
+// For openNavifation and clossNavigation
 const nav = document.querySelector('nav');
-const hamburgerBtnExit = document.getElementById('hamburger-exit-cta');
+const openNavigation = document.querySelector('.open-navigation');
+const clossNavigation = document.querySelector('.closs-navigation');
+const iframe = document.querySelectorAll('iframe');
+const navLink = document.querySelector('.nav-link');
+const sectionDirectors = document.querySelector('.directors');
+console.log(iframe);
 
-if (hamburgerBtn != null && hamburgerBtnExit != null) {
-	hamburgerBtn.addEventListener('click', () => {
-		nav.classList.add('show-manu');
-	});
-
-	hamburgerBtnExit.addEventListener('click', () => {
-		nav.classList.remove('show-manu');
-	});
+for (let i = 0; i < iframe.length; i++) {
+	iframe[i].loading = 'lazy';
 }
+
+// Open nav and closs
+const hamburgerMenu = function (openNav, clossNav) {
+	if (openNav !== null && clossNav !== null) {
+		openNav.addEventListener('click', () => {
+			nav.style.height = '100vh';
+			openNav.classList.add('hide');
+			clossNav.classList.remove('hide');
+			navLink.classList.remove('hide');
+		});
+
+		clossNav.addEventListener('click', () => {
+			nav.style.height = '0vh';
+			clossNav.classList.add('hide');
+			openNav.classList.remove('hide');
+			navLink.classList.add('hide');
+		});
+	}
+};
+hamburgerMenu(openNavigation, clossNavigation);
 
 // For clients slider
 $('.clients-slider').slick({
@@ -52,74 +70,62 @@ $('.clients-slider').slick({
 	],
 });
 
-// Jquery for active headings
-// $(document).ready(function () {
-// 	// First active heading
-// 	$('.heading-one').click(function () {
-// 		$('.bottom-line').addClass('bottom-active1');
-// 		$('.bottom-line').removeClass('bottom-active2 bottom-active3 bottom-active4 bottom-active5');
-// 	});
+// For our work page //
+// Select all the list item and the video container
+const listItem = document.querySelectorAll('.heading-wrapper ul .item');
+console.log(listItem);
 
-// 	// Second active heading
-// 	$('.heading-two').click(function () {
-// 		$('.bottom-line').addClass('bottom-active2');
-// 		$('.bottom-line').removeClass('bottom-active1 bottom-active3 bottom-active4 bottom-active5');
-// 	});
+const ourWorkVideos = document.querySelectorAll('.tvc, .digital, .documentary, .events, .corporate');
+console.log(ourWorkVideos);
 
-// 	// Third active heading
-// 	$('.heading-three').click(function () {
-// 		$('.bottom-line').addClass('bottom-active3');
-// 		$('.bottom-line').removeClass('bottom-active1 bottom-active2 bottom-active4 bottom-active5');
-// 	});
+// Function to check the current active item
+const checkCrntAct = function (list) {
+	list = document.getElementsByClassName('active');
+	console.log(list);
 
-// 	// Fourth active heading
-// 	$('.heading-four').click(function () {
-// 		$('.bottom-line').addClass('bottom-active4');
-// 		$('.bottom-line').removeClass('bottom-active1 bottom-active2 bottom-active3 bottom-active5');
-// 	});
+	for (let i = 0; i < list.length; i++) {
+		console.log(list[i]);
+		list[i].classList.remove('active');
+	}
+};
 
-// 	// Fifth active heading
-// 	$('.heading-five').click(function () {
-// 		$('.bottom-line').addClass('bottom-active5');
-// 		$('.bottom-line').removeClass('bottom-active1 bottom-active2 bottom-active3 bottom-active4');
-// 	});
-// });
+// Function to check current active video
+const checkCrntActVideo = function (video) {
+	video = document.getElementsByClassName('video-active');
 
-// Jquery for active headings and show videos according to the active heading
-$(document).ready(function () {
-	$('.heading-one').click(function () {
-		$('.heading-one').addClass('active');
-		$('.heading-two, .heading-three, .heading-four, .heading-five').removeClass('active');
-		$('.tvc').addClass('video-active');
-		$('.digital, .documentary, .events, .corporate').removeClass('video-active');
-	});
+	for (let i = 0; i < video.length; i++) {
+		console.log(video[i]);
+		video[i].classList.remove('video-active');
+	}
+};
 
-	$('.heading-two').click(function () {
-		$('.heading-two').addClass('active');
-		$('.heading-one, .heading-three, .heading-four, .heading-five').removeClass('active');
-		$('.digital').addClass('video-active');
-		$('.tvc, .documentary, .events, .corporate').removeClass('video-active');
-	});
+//Loop on the listItem
+listItem.forEach(function (item) {
+	item.addEventListener('click', function (event) {
+		checkCrntAct(listItem);
+		checkCrntActVideo(ourWorkVideos);
+		event.currentTarget.className += ' active';
 
-	$('.heading-three').click(function () {
-		$('.heading-three').addClass('active');
-		$('.heading-one, .heading-two, .heading-four, .heading-five').removeClass('active');
-		$('.documentary').addClass('video-active');
-		$('.tvc, .digital, .events, .corporate').removeClass('video-active');
-	});
+		// Switch statement
+		const activeVideo = event.target.innerHTML;
 
-	$('.heading-four').click(function () {
-		$('.heading-four').addClass('active');
-		$('.heading-one, .heading-two, .heading-three, .heading-five').removeClass('active');
-		$('.events').addClass('video-active');
-		$('.tvc, .digital, .documentary, .corporate').removeClass('video-active');
-	});
-
-	$('.heading-five').click(function () {
-		$('.heading-five').addClass('active');
-		$('.heading-one, .heading-two, .heading-three, .heading-four').removeClass('active');
-		$('.corporate').addClass('video-active');
-		$('.tvc, .digital, .documentary, .events').removeClass('video-active');
+		switch (activeVideo) {
+			case 'TVC':
+				document.querySelector('.tvc').classList.add('video-active');
+				break;
+			case 'DIGITAL':
+				document.querySelector('.digital').classList.add('video-active');
+				break;
+			case 'DOCUMENTARY':
+				document.querySelector('.documentary').classList.add('video-active');
+				break;
+			case 'EVENTS':
+				document.querySelector('.events').classList.add('video-active');
+				break;
+			case 'CORPORATE':
+				document.querySelector('.corporate').classList.add('video-active');
+				break;
+		}
 	});
 });
 
@@ -154,11 +160,11 @@ Mouseout = (el) => {
 	yt_object.pauseVideo();
 };
 
-// Directors
-let directors = [
+//*-----------Page: directors ----------*//
+const directors = [
 	{
 		name: 'Minalee Madanayake',
-		image: '/assert/images/directors/img-minalee-madanayake.png',
+		image: './assert/images/directors/img-minalee-madanayake.png',
 		instagram: 'https://www.instagram.com/mohamadrawshan/',
 		facebook: 'https://www.instagram.com/mohamadrawshan/',
 		bodyText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis debitis fugiat',
@@ -206,53 +212,50 @@ let directors = [
 	},
 ];
 
-directors.forEach(function (director) {
-	//Create directors name list
-	let directorsNameList = document.createElement('ul');
-	let li = document.createElement('li');
-	li.textContent = director.name;
-	directorsNameList.appendChild(li);
-	document.getElementById('directors-name').appendChild(directorsNameList);
-	//When the director name was clicked
-	li.addEventListener('click', function () {
-		//Underline the director name
+// Create directors
+const displayDirectors = function (directors) {
+	directors.forEach(function (director) {
+		//Create directors name list
+		const directorsNameList = document.createElement('ul');
+		const li = document.createElement('li');
+		li.textContent = director.name;
+		directorsNameList.appendChild(li);
+		document.getElementById('directors-name').appendChild(directorsNameList);
+		//When the director name was clicked
+		li.addEventListener('click', function () {
+			const list = document.getElementsByClassName('active');
+			for (const item of list) {
+				item.classList.remove('active');
+			}
+			li.className = 'active';
 
-		//Hide the VMLK logo
-		let dPLogo = document.getElementById('hide-element');
-		dPLogo.classList.add('hide');
+			//Hide the VMLK logo
+			const dPLogo = document.getElementById('hide-element');
+			dPLogo.classList.add('hide');
 
-		//Director image
-		let directorImage = document.getElementById('director-image');
-		directorImage.src = director.image; //Director image is hidden until the  user click the director
-		directorImage.classList.remove('hide');
+			//Director image
+			const directorImage = document.getElementById('director-image');
+			directorImage.src = director.image; //Director image is hidden until the  user click the director
+			directorImage.classList.remove('hide');
 
-		//Instagram and Facebook icon
-		let instagram = document.getElementById('director-instagram');
-		instagram.href = director.instagram;
-		console.log(instagram.href);
+			//Instagram and Facebook icon
+			const instagram = document.getElementById('director-instagram');
+			instagram.href = director.instagram;
+			console.log(instagram.href);
 
-		let facebook = document.getElementById('director-facebook');
-		facebook.href = director.facebook;
-		console.log(facebook.href);
+			const facebook = document.getElementById('director-facebook');
+			facebook.href = director.facebook;
+			console.log(facebook.href);
 
-		let socialIcon = document.getElementById('director-social-icon');
-		socialIcon.classList.remove('hide');
-		//Director body text
-		let bodyText = document.getElementById('director-body-text');
-		bodyText.textContent = director.bodyText;
+			const socialIcon = document.getElementById('director-social-icon');
+			socialIcon.classList.remove('hide');
+			//Director body text
+			const bodyText = document.getElementById('director-body-text');
+			bodyText.textContent = director.bodyText;
+		});
 	});
-});
+};
 
-// function myFunction() {
-// 	directors.forEach(function (director) {
-// 		//Create director image
-// 		let directorImage = document.createElement('img');
-// 		directorImage.src = director.image;
-// 		document.getElementById('director-image').appendChild(directorImage);
-
-// 		//Create director body text
-// 		let bodyText = document.createElement('p');
-// 		bodyText.textContent = director.bodyText;
-// 		document.getElementById('director-body-text').appendChild(bodyText);
-// 	});
-// }
+if (sectionDirectors !== null) {
+	displayDirectors(directors);
+}
